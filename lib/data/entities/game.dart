b698890b489c57d1c334fe44769470ef9f17c1e6
Vincent Brocheton <vbrocheton@fcdigital.fr>
2022-05-24 12:37:20 +0200

    Entities Game & User

---
+++ lib/data/entities/game.dart
@@ -0,0 +1,25 @@
+import 'package:flutter_motus/data/entities/user.dart';
+import 'package:hive/hive.dart';
+
+@HiveType(typeId: 2)
+class Game {
+  @HiveField(0)
+  DateTime? startDate;
+
+  @HiveField(1)
+  DateTime? endDate;
+
+  @HiveField(2)
+  User? player;
+
+  @HiveField(3)
+  double? score;
+
+  @HiveField(4)
+  List<String>? tries;
+
+  @HiveField(5)
+  int limitTries = 5;
+
+  Game({required this.startDate, required this.player});
+}
